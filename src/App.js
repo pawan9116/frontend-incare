0369593534dbe419933de16c715f27b7472cb9d8
pawan9116 <pawanraip4@gmail.com>
2022-01-12 15:57:24 +0530

    feat added immunisation

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ import Homepage from './components/Home'
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
+import Immunisation from './components/Immunisation';
 
 
 function App() {
   return (
     <Router>
     <Header/>
-    <main className='py-3'>
-      <Container>
+    <main className='py-3'>      
         <Routes>
         <Route path='/' exact element={<Homepage/>}/>
         <Route path='/login' element={<Login/>} exact />
         <Route path='/signup' element={<Signup/>} exact />
         <Route path='/dashboard' element={<Dashboard/>} exact />
+        <Route path='/immunisation' element={<Immunisation/>} exact />
         </Routes>
-      </Container>
     </main>
     <Footer/>
     </Router>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="p-5 bg-dark text-white text-center position-relative">
       <Container>
         <Row>
           <Col className='text-center py-3'>Copyright &copy; Incare</Col>
